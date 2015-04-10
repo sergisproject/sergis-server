4d8d07d247c306b6d5e9b4101f249505f9d73b8d
Jake Hartz <jhartz@outlook.com>
2015-04-10 10:12:20 -0400

    Added better descriptions

--- config.js
+++ config.js
@@ -15,6 +15,7 @@ var path = require("path");
 
 /**
  * Arguments passed via the command line (argv).
+ * @type {Object.<string, (string|boolean)>}
  */
 var args = {};
 (function () {
@@ -51,57 +52,120 @@ var argdata = [
  * SerGIS Server configuration.
  */
 var config = module.exports = {
-    /** Default server port */
+    /**
+     * Default server port.
+     * @type {number}
+     */
     PORT: process.env.PORT || 3000,
     
-    /** MongoDB server */
+    /**
+     * MongoDB server path.
+     * @type {string}
+     */
     MONGO_SERVER: "mongodb://localhost:27017/sergis-server",
     
-    /** Session secret (NOTE: This should be changed from the default!!) */
+    /**
+     * Session secret.
+     * NOTE: This should be changed from the default!
+     * @type {string}
+     */
     SESSION_SECRET: "put-your-random-sergis-session-secret-here",
     
     ///////////////////////////////////////////////////////////////////////////
     // ARGUMENT-OVERRIDDEN CONFIG
     
-    /** Whether to start the HTTP server */
+    /**
+     * Whether to start the HTTP server.
+     * @type {boolean}
+     */
     ENABLE_HTTP_SERVER: !!args["start-http-server"],
     
-    /** Whether to start the WebSockets (socket.io) server */
+    /**
+     * Whether to start the WebSockets (socket.io) server.
+     * @type {boolean}
+     */
     ENABLE_SOCKET_SERVER: !!args["start-socket-server"],
     
-    /** Origin for the HTTP server (only set if running separately as the socket.io server) */
+    /**
+     * Origin for the HTTP server.
+     * (Set to "" if the HTTP server is running in the same node instance as
+     *  the socket.io server.)
+     * @type {string}
+     */
     HTTP_ORIGIN: args["http-server-origin"] || "",
     
-    /** Origin for the socket.io server (set to empty string if same as http server) */
+    /**
+     * Origin for the socket.io server.
+     * (Set to "" if the socket.io server is running in the same node instance
+     *  as the HTTP server.)
+     * @type {string}
+     */
     SOCKET_ORIGIN: args["socket-server-origin"] || "",
     
-    /** The prefix to the path (i.e. if someone is serving us at /my-web-game/..., this would be "/my-web-game") */
+    /**
+     * The prefix to the path.
+     * Example: if some proxy is serving us at /my-web-game/..., this would be
+     * "/my-web-game"
+     * @type {string}
+     */
     HTTP_PREFIX: args["http-server-prefix"] || "",
     
-    /** The prefix to the socket.io server, if hosted through some sort of proxy that adds to the beginning of the path
-        (i.e. if the socket.io.js path is /my-web-game/socket.io/socket.io.js, this would be "/my-web-game") */
+    /**
+     * The prefix to the socket.io server, if hosted through some sort of proxy
+     * that adds to the beginning of the path.
+     * Example: if the socket.io.js path is /my-web-game/socket.io/socket.io.js,
+     * this would be "/my-web-game".
+     * @type {string}
+     */
     SOCKET_PREFIX: args["socket-server-prefix"] || "",
     
     ///////////////////////////////////////////////////////////////////////////
     // DATA DIRECTORIES
     
-    /** Templates directory */
+    /**
+     * Templates directory.
+     * @type {string}
+     */
     TEMPLATES_DIR: path.join(__dirname, "templates"),
     
-    /** sergis-client resources directory (mapped to http://this-nodejs-server/client-lib/...) */
+    /**
+     * sergis-client resources directory.
+     * (mapped to http://this-nodejs-server/client-lib/...)
+     * @type {string}
+     */
     CLIENT_RESOURCES_DIR: path.join(__dirname, "sergis-client", "lib"),
     
-    /** sergis-client JavaScript files to concatenate and minify (in this order) */
+    /**
+     * sergis-client JavaScript files to concatenate and minify (in this order)
+     * if the sergis-server backend is used.
+     * @type {Array.<string>}
+     */
     CLIENT_RESOURCES_JS: ["main.js", "frontends/arcgis.js", "backends/sergis-server.js"],
+    
+    /**
+     * sergis-client JavaScript files to concatenate and minify (in this order)
+     * if the local backend is used.
+     * @type {Array.<string>}
+     */
     CLIENT_RESOURCES_JS_LOCAL: ["main.js", "frontends/arcgis.js", "backends/local.js"],
     
-    /** sergis-client index file (path to index.html) */
+    /**
+     * sergis-client index file (path to index.html).
+     * @type {string}
+     */
     CLIENT_INDEX: path.join(__dirname, "sergis-client", "index.html"),
     
-    /** sergis-author resources directory (mapped to http://this-nodejs-server/author-lib/...) */
+    /**
+     * sergis-author resources directory.
+     * (mapped to http://this-nodejs-server/author-lib/...)
+     * @type {string}
+     */
     AUTHOR_RESOURCES_DIR: path.join(__dirname, "sergis-author"),
     
-    /** sergis-author JavaScript files to concatenate and minify (in this order) */
+    /**
+     * sergis-author JavaScript files to concatenate and minify (in this order).
+     * @type {Array.<string>}
+     */
     AUTHOR_RESOURCES_JS: [
         "author-main.js", "author-ask.js",
         "author-json.js", "author-games.js", "author-table.js",
@@ -109,7 +173,10 @@ var config = module.exports = {
         "author-backend_sergis-server.js"
     ],
     
-    /** sergis-author index file (path to index.html) */
+    /**
+     * sergis-author index file (path to index.html).
+     * @type {string}
+     */
     AUTHOR_INDEX: path.join(__dirname, "sergis-author", "index.html"),
 
     ///////////////////////////////////////////////////////////////////////////
@@ -119,15 +186,25 @@ var config = module.exports = {
      * Whether to temporarily disable login and assume everyone is a logged-in
      * admin. Can be used when first setting up the server before there are any
      * accounts. MUST BE SET TO `false` ONCE THE SERVER IS SET UP!!
+     * @type {boolean}
      */
     ASSUME_ADMIN: false,
     
-    /** Regex for things that must be URL-safe (i.e. usernames and game names) */
+    /**
+     * Regex for things that must be URL-safe (i.e. usernames and game names).
+     * @type {RegExp}
+     */
     URL_SAFE_REGEX: /^[A-Za-z0-9~$"':;,.\-_]+$/,
     
-    /** Human-readable character list for URL_SAFE_REGEX */
+    /**
+     * Human-readable character list for URL_SAFE_REGEX.
+     * @type {string}
+     */
     URL_SAFE_REGEX_CHARS: "~ $ \" ' : ; , . - _",
 
-    /** Information about command-line arguments that can be passed to server.js */
+    /**
+     * Information about command-line arguments that can be passed to server.js.
+     * @type {Object.<string, (string|boolean)>}
+     */
     argdata: argdata
 };
